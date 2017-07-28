7291952f6d45729df855d4389b4ccaa9d79f432e
Michael Kapelko <kornerr@gmail.com>
2017-07-28 22:26:15 +0700

    Add color table

--- cfg.lua
+++ cfg.lua
@@ -2,3 +2,4 @@
 width = 200
 height = 300
 background = { r = 0.3, g = 0.1, b = 0 }
+foreground = BLUE
--- main.cpp
+++ main.cpp
@@ -3,6 +3,47 @@
 
 #include "lua.hpp"
 
+const int MAX_COLOR = 255;
+
+struct ColorTable
+{
+    const char *name;
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
+
+ColorTable colorTable[] =
+{
+    {"WHITE", MAX_COLOR, MAX_COLOR, MAX_COLOR},
+    {"BLACK", 0, 0, 0},
+    {"RED", MAX_COLOR, 0, 0},
+    {"GREEN", 0, MAX_COLOR, 0},
+    {"BLUE", 0, 0, MAX_COLOR},
+    {0, 0, 0, 0}, // Sentinel.
+};
+
+// Assume table is at the top of the stack, i.e., it has '-1' index.
+int tableField(lua_State *L, const char *key)
+{
+    int result = -1;
+    lua_pushstring(L, key);
+    lua_gettable(L, -2);
+    if (!lua_isnumber(L, -1))
+    {
+        printf(
+            "ERROR. Table field '%s' must be a number: '%s'",
+            key,
+            lua_tostring(L, -1));
+    }
+    else
+    {
+        result = (int)lua_tonumber(L, -1) * MAX_COLOR;
+    }
+    lua_pop(L, 1);
+    return result;
+}
+
 bool loadLuaFile(lua_State *L, const char *fileName)
 {
     if (luaL_loadfile(L, fileName) || lua_pcall(L, 0, 0, 0))
@@ -11,6 +52,7 @@ bool loadLuaFile(lua_State *L, const char *fileName)
             "ERROR. Cannot load Lua file '%s': '%s'\n",
             fileName,
             lua_tostring(L, -1));
+        lua_pop(L, 1);
         return false;
     }
     return true;
@@ -69,7 +111,6 @@ int main(int argc, char *argv[])
 
     // Close lua state.
     lua_close(L);
-
 
     return 0;
 }
